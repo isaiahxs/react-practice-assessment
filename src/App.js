b290b2603779cefb68bf66d1e3020ca58587d23a
isaiahxs <isaiahsinnathamby@gmail.com>
2023-03-11 15:09:27 -0500

    completed phase 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import fruits from "./mockData/fruits.json";
+import FruitsIndex from "./components/FruitsIndex";
 
 function App() {
   return (
     <>
       <h1>Welcome to Fruits App</h1>
+      {/* render FruitsIndex inside of App
+        pass in array of fruits from mockdata as a prop called fruits */}
+      <FruitsIndex fruits={fruits}/>
     </>
   );
 }
--- src/components/FruitsIndex.js
+++ src/components/FruitsIndex.js
@@ -1,5 +1,21 @@
-function FruitsIndex() {
-  return null;
+import { Link } from "react-router-dom";
+
+//i'm assuming i'll need to put fruits as a prop in the parentheses?
+function FruitsIndex(props) {
+  return (
+    <div className='fruits-index'>
+      <h2>Fruits Index</h2>
+      {/* render links to the fruit show pages for ALL of the fruits passed in as props */}
+      {/* each link should be formatted as /fruits/:fruitId with :fruitId replaced with a fruit's id */}
+      {/* the visual content of each link should be that fruit's name */}
+      {props.fruits.map((fruit) => (
+        <div key={fruit.id}>
+          <Link to={`/fruits/${fruit.id}`}>{fruit.name}</Link>
+        </div>
+      ))}
+
+    </div>
+  )
 }
 
 export default FruitsIndex;

42b7f6c8ddfbfbe78bec4e3dd260aab6345a4ef0
isaiahxs <isaiahsinnathamby@gmail.com>
2023-03-11 19:25:22 -0500

    completed phase 4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import FruitsIndex from "./components/FruitsIndex";
 import FruitForm from "./components/FruitForm";
 import FavoriteFruit from "./components/FavoriteFruit";
 import SetFavoriteFruit from "./components/SetFavoriteFruit";
+import FruitShow from './components/FruitShow';
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -10,10 +12,34 @@ function App() {
       <h1>Welcome to Fruits App</h1>
       {/* render FruitsIndex inside of App
         pass in array of fruits from mockdata as a prop called fruits */}
-      <FruitsIndex fruits={fruits}/>
-      <FruitForm fruits={fruits}/>
-      <FavoriteFruit fruits={fruits}/>
-      <SetFavoriteFruit fruits={fruits}/>
+        {/* need to use Switch for the "page not found" */}
+        <Switch>
+          {/* this will catch links redirecting routes to this path */}
+          <Route exact path='/'>
+            <FruitsIndex fruits={fruits}/>
+          </Route>
+
+          <Route path='/fruits/new'>
+            <FruitForm fruits={fruits}/>
+          </Route>
+
+          <Route path='/fav-fruit'>
+            <FavoriteFruit fruits={fruits}/>
+          </Route>
+
+          <Route path='/set-fav-fruit'>
+            <SetFavoriteFruit fruits={fruits}/>
+          </Route>
+
+          <Route path='/fruits/:fruitId'>
+            <FruitShow />
+          </Route>
+
+          {/* this will catch everything else */}
+          <Route path='/'>
+            <p>Page Not Found</p>
+          </Route>
+        </Switch>
     </>
   );
 }

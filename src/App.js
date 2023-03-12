2f1797fa71ed218b3fc8b3a5b2417c4530dd574e
isaiahxs <isaiahsinnathamby@gmail.com>
2023-03-11 20:52:43 -0500

    completed all phases

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
           </Route>
 
           <Route path='/fruits/:fruitId'>
-            <FruitShow />
+            <FruitShow fruits={fruits}/>
           </Route>
 
           {/* this will catch everything else */}
--- src/components/FruitShow.js
+++ src/components/FruitShow.js
@@ -1,5 +1,30 @@
-function FruitShow() {
-  return null;
+import { useParams } from 'react-router-dom';
+
+function FruitShow({fruits}) {
+
+  //from the url, we know that fruitId is a string
+  const {fruitId} = useParams();
+
+  //to get fruit's name
+    ///go thru array of objects
+  const {name, color, sweetness, seeds} = fruits.find(fruit => fruit.id === fruitId)
+
+  return (
+    <div className='fruit-show'>
+      <h2>
+        {name}
+        {/* name of the fruit with id of 1 */}
+      </h2>
+      <ul>
+        <li>{color}</li>
+        <li>{sweetness}</li>
+        <li>{seeds && seeds}</li>
+      </ul>
+
+
+
+    </div>
+  )
 }
 
 export default FruitShow;
